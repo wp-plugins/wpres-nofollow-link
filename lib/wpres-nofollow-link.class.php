6412d661764a6ca14dd5c04b672e7bbbd5096a2e
wpres.net <wpres.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 18:55:34 +0000

    Version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wpres-nofollow-link/tags/1.0.0@1159064 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/wpres-nofollow-link.class.php
+++ lib/wpres-nofollow-link.class.php
@@ -1,47 +1,34 @@
 <?php
  
 if (!defined('ABSPATH')) {
-    exit;
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit();
 }
+define('WPRES_NOFOLLOW_LINK_PATH', plugin_dir_path(WPRES_NOFOLLOW_LINK_FILE));
+define('WPRES_NOFOLLOW_LINK_BASENAME', plugin_basename(WPRES_NOFOLLOW_LINK_FILE));
+define('WPRES_NOFOLLOW_LINK_URL', plugin_dir_url(WPRES_NOFOLLOW_LINK_FILE));
 class WPResNoFollowLink {
-    private $rootUrl;
-    private $rootPath;
-    public function __construct($rootPath, $rootUrL) {
-
-        $this->rootPath = $rootPath;
-        $this->rootUrl = $rootUrL;
+    public function __construct() {
         
 
         $this->addActions();
+        
 
         if (is_admin()) {
-            $this->addActionsAdmin();
+            include_once (WPRES_NOFOLLOW_LINK_PATH . '/lib/wpres-nofollow-link-admin.class.php');
+            $WPResNoFollowLinkAdmin = new WPResNoFollowLinkAdmin();
         }
+        
     }
     public function addActions() {
 
         add_action( 'init', array(&$this, 'loadPluginTextdomain'));
-    }
-    public function addActionsAdmin() {
-        add_action( 'admin_enqueue_scripts', array(&$this, 'changeWplink'), 99999 );
     }
     
     public function loadPluginTextdomain() {
     $locale = apply_filters('plugin_locale', get_locale(), 'wpres-nofollow-link');
     load_textdomain('wpres-nofollow-link', WP_LANG_DIR . '/plugins/wpres-nofollow-link' . $locale . '.mo');
     load_plugin_textdomain('wpres-nofollow-link', FALSE, '/wpres-nofollow-link/languages/');        
-    }
-    public function changeWplink() {
-        wp_deregister_script('wplink');
-        $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
-        wp_register_script('wplink', $this->rootUrl . '/js/wplink' . $suffix . '.js', array('jquery'), false, 1);
-        wp_localize_script('wplink', 'wpLinkL10n', array(
-            'title' => __('Insert/edit link'),
-            'update' => __('Update'),
-            'save' => __('Add Link'),
-            'noTitle' => __('(no title)'),
-            'noMatchesFound' => __('No results found.'),
-            'noFollow' => __('add <code>rel="nofollow"</code> to link', 'wpres-nofollow-link')
-        ));
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,16 +1,25 @@
 === WPRes - nofollow link ===
 Contributors: wpres.net
 Plugin URI: https://wordpress.org/plugins/wpres-nofollow-link/
-Stable tag: 0.9.1
+Stable tag: 1.0.0
 Tags: nofollow,link,links,meta,insert,rel nofollow,seo,checkbox,popup
 Requires at least: 4.2
-Tested up to: 4.2.1
+Tested up to: 4.2.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Adds a checkbox in the insert link popup box for including rel="nofollow" in links
 
 == Description ==
-Adds a checkbox in the insert link popup box for including rel="nofollow" in links
+Adds a checkbox in the insert link popup box for including rel="nofollow" in links.
+
+= Languages =
+
+* english (standard)
+* german / deutsch (de_DE)
+* russian / pоссия (ru_RU) - Thanks to Flector
+
+= Support =
+If you have Questions please send a mail to: [support@wpres.net](mailto:support@wpres.net)
 
 == Installation ==
 1. Download the latest zip file and extract the `wpres-nofollow` directory.
@@ -25,17 +34,18 @@ No FAQ yet. Just activate the plugin and it works...
 
 == Changelog ==
 
-= 0.9.1 =
-* Removed a Space in Source to avoid a error in activating the plugin
+= 1.0.0 (2015-05-12) =
+* beta is over ;-)
+* NEW: Added L10n Russian - Thanks to Flector
+* CODE: Some internal improvements
 
-= 0.9.0 =
-* Added i18n
-* Added L10n German and english
-* small Code Improvements
+= 0.9.1 (2015-04-30) =
+* BUGFIX: Removed a space in source to avoid a error in activating the plugin
 
-= 0.5.0 =
-* First release, first beta
+= 0.9.0 (2015-04-30) =
+* NEW: Added i18n
+* NEW: Added L10n german and english
+* CODE: Small code improvements
 
-
-== Support ==
-If you have Questions please send a mail to: [support@wpres.net](mailto:support@wpres.net)
+= 0.5.0 (2015-04-29) =
+* Plugin goes online
